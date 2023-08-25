d84c79be912cca7d0d1caff7006d6385ed78751c
Chester Corpuz <chezramosc@gmail.com>
2023-08-24 21:30:04 -1000

    Modification of gitignore and creation of node server

---
+++ Stock_Data/data_server.mjs
@@ -0,0 +1,16 @@
+require('dotenv').config();
+import { restClient } from '@polygon.io/client-js';
+//console.log(process.env.STOCK_KEY) for testing purposes
+
+//connect to Polygon
+const rest = restClient(process.env.STOCK_KEY)
+
+//simple start to determine algo
+//calls Poly API to grab APPL data by day and log to console
+rest.stocks.aggregates("AAPL", 1, "day", "2023-01-01", "2023-04-14")
+.then((data) => {
+	console.log(data);
+})
+.catch(e => { //promise returns an error
+	console.error('An error happened:', e);
+});
