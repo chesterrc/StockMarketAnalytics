e7319d96e6038e43b2cebd73e841510dea4d2b47
Chester Corpuz <chezramosc@gmail.com>
2023-08-24 21:00:50 -1000

    Changed to JS server side because it is easier to extract data_server

--- Stock_Data/main.py
+++ Stock_Data/main.py
@@ -11,6 +11,17 @@ load_dotenv()
 
 #creat new client with API key
 sen_key = os.getenv('sen_key')
+client = RESTClient(sen_key)
+
+ticker = "AAPL"
+
+# List Aggregates (Bars)
+aggs = []
+for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="day", from_="2023-01-01", to="2023-06-13", limit=50000):
+    aggs.append(a)
+
+print(aggs)
+
 
 #class for importing stock data
 class stock():
